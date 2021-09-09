895ac060c0d4403bafd71be73052b98f1525af30
Arjun Khanduri <arjun99.khanduri@gmail.com>
2021-09-09 20:48:08 +0530

    Made title dynamic

--- src/components/Details/Details.tsx
+++ src/components/Details/Details.tsx
@@ -3,11 +3,11 @@ import { Card, CardHeader, CardContent, Typography } from '@material-ui/core';
 import { Doughnut } from 'react-chartjs-2';
 import useStyes from './styles';
 
-const Details = () => {
+const Details = ({ title }: { title: string }) => {
     const classes = useStyes();
     return (
-        <Card className={classes.income}>
-            <CardHeader title="Income" />
+        <Card className={title === 'Income' ? classes.income : classes.expense}>
+            <CardHeader title={title} />
             <CardContent>
                 <Typography variant="h5">
                     INR 100

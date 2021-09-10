87ff19aa955a08f2ffb09b6730efd9fe984004a9
Arjun Khanduri <arjun99.khanduri@gmail.com>
2021-09-10 23:46:00 +0530

    Created input form for Main component

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Card, CardHeader, CardContent, Typography, Grid, Divider } from '@material-ui/core';
 import useStyles from './styles'
+import Form from './Form/Form';
 
 const Main = () => {
     const classes = useStyles()
@@ -13,6 +14,7 @@ const Main = () => {
                     Try saying: Add income for INR 100...
                 </Typography>
                 <Divider />
+                <Form />
             </CardContent>
             <CardContent className={classes.cardContent}>
                 <Grid container spacing={2}>

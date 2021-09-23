5c0afd099cfacb9b1ea938cc302748a4ad20b2a6
Arjun Khanduri <arjun99.khanduri@gmail.com>
2021-09-23 12:21:35 +0530

    Removed TS

--- src/context/context.js
+++ src/context/context.js
@@ -1,11 +1,11 @@
-import React, { useReducer, createContext, Dispatch } from 'react'
+import React, { useReducer, createContext } from 'react'
 import contextReducer from './contextReducer'
 const initialState = []
 
 export const ExpenseTrackerContext = createContext(initialState)
 
 
-export const Provider = (children) => {
+export const Provider = ({children}) => {
     const [transactions, dispatch] = useReducer(contextReducer, initialState)
 
 

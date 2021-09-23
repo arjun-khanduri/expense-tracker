0cf707637abf02c1867f1c5517390b4179c03903
Arjun Khanduri <arjun99.khanduri@gmail.com>
2021-09-23 18:07:54 +0530

    Added date formatting

--- src/components/Main/Form/Form.js
+++ src/components/Main/Form/Form.js
@@ -14,12 +14,13 @@ import {
     from '@material-ui/core'
 import useStyles from './styles'
 import { incomeCategories, expenseCategories } from '../../../constants/categories';
+import formatDate from '../../../utils/formatDate';
 
 const initialState = {
     amount: '',
     category: '',
     type: 'Income',
-    date: new Date()
+    date: formatDate(new Date())
 };
 
 
@@ -64,10 +65,10 @@ const Form = () => {
                 <TextField type="number" label="Amount" fullWidth value={formData.amount} onChange={(e) => setFormData({ ...formData, amount: e.target.value })} />
             </Grid>
             <Grid item xs={6}>
-                <TextField type="date" label="Date" fullWidth value={formData.date} onChange={(e) => setFormData({ ...formData, date: e.target.value })} />
+                <TextField type="date" label="date" fullWidth value={formData.date} onChange={(e) => setFormData({ ...formData, date: formatDate(e.target.value) })} />
             </Grid>
             <Button className={classes.button} variant="outlined" color="primary" fullWidth onClick={createTransaction}>
-                Create
+                Create Transaction
             </Button>
         </Grid>
     )

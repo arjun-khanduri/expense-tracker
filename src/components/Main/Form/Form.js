2b5bea7a42b7be68b736c3b5d4d33541f1801a03
Arjun Khanduri <arjun99.khanduri@gmail.com>
2021-09-27 13:21:34 +0530

    Added error check for creating transactions

--- src/components/Main/Form/Form.js
+++ src/components/Main/Form/Form.js
@@ -19,7 +19,7 @@ import formatDate from '../../../utils/formatDate';
 const initialState = {
     amount: 1,
     category: '',
-    type: 'Income',
+    type: '',
     date: formatDate(new Date())
 };
 
@@ -32,6 +32,8 @@ const Form = () => {
     const selectedType = formData.type === 'Income' ? incomeCategories : expenseCategories
 
     const createTransaction = () => {
+        if(Number.isNaN(Number(formData.amount)) || !formData.date.includes('-') || formData.type === '' || formData.category === '')
+            return;
         const transaction = { ...formData, amount: formData.amount, id: uuidv4() }
         addTransaction(transaction)
         setFormData(initialState)
@@ -47,7 +49,11 @@ const Form = () => {
             <Grid item xs={6}>
                 <FormControl fullWidth>
                     <InputLabel>Type</InputLabel>
-                    <Select value={formData.type} onChange={(e) => setFormData({ ...formData, type: e.target.value })}>
+                    <Select
+                        value={formData.type}
+                        onChange={(e) => setFormData({ ...formData, type: e.target.value })}
+                        required>
+                        <MenuItem>Choose...</MenuItem>
                         <MenuItem value="Income">Income</MenuItem>
                         <MenuItem value="Expense">Expense</MenuItem>
                     </Select>
@@ -56,16 +62,31 @@ const Form = () => {
             <Grid item xs={6}>
                 <FormControl fullWidth>
                     <InputLabel>Category</InputLabel>
-                    <Select value={formData.category} onChange={(e) => setFormData({ ...formData, category: e.target.value })}>
+                    <Select
+                        value={formData.category}
+                        onChange={(e) => setFormData({ ...formData, category: e.target.value })}
+                        required>
                         {selectedType.map((category) => <MenuItem key={category.type} value={category.type}>{category.type}</MenuItem>)}
                     </Select>
                 </FormControl>
             </Grid>
             <Grid item xs={6}>
-                <TextField type="number" label="Amount" fullWidth value={formData.amount} InputProps={{ inputProps: { min: 1 } }} onChange={(e) => setFormData({ ...formData, amount: e.target.value })} />
+                <TextField
+                    type="number"
+                    label="Amount"
+                    fullWidth value={formData.amount}
+                    InputProps={{ inputProps: { min: 1 } }}
+                    onChange={(e) => setFormData({ ...formData, amount: e.target.value })}
+                    required />
             </Grid>
             <Grid item xs={6}>
-                <TextField type="date" label="date" fullWidth value={formData.date} onChange={(e) => setFormData({ ...formData, date: formatDate(e.target.value) })} />
+                <TextField
+                    type="date"
+                    label="Date"
+                    fullWidth
+                    value={formData.date}
+                    onChange={(e) => setFormData({ ...formData, date: formatDate(e.target.value) })}
+                    required />
             </Grid>
             <Button className={classes.button} variant="outlined" color="primary" fullWidth onClick={createTransaction}>
                 Create Transaction
